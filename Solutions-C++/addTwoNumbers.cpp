ade121c0b5348631e1ac3f14ac8008adbb9f9df5
Reshu <vreshu1998@gmail.com>
2022-10-17 01:16:58 +0530

    added code in c++ to add two numbers

---
+++ Solutions-C++/addTwoNumbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+
+    cin>>num1>>num2;
+
+    int ans = num1 + num2;
+
+    cout<<" Sum of Numbers entere is "<<ans;
+
+    return 0;
+
+
+    
+} 
+
